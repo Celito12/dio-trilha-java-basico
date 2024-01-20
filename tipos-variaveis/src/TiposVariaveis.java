5a106a15248c373db774a02351a0772f9194f493
Marcelo Telles <marceloniat12telles@gmail.com>
2024-01-20 17:46:15 -0300

    final da aula

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,19 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        
+        //tipos primitivos
+        
+        String meuNome = "Marcelo Telles";
+        double salarioMinimo = 2500;
+        byte idade = 123;
+        short ano = 2024;
+        int cep = 50610290;
+        long cpf = 12345678L; // se começar com zero, talvez tenha que ser outro tipo
+        float pi = 3.14F;
+        double salario = 1275.33;
+
+        int numero = 1;        
+        numero = 2;
+        final double VALOR_DE_PI = 3.14; //esse valor nunca vai mudar
+    } 
 }
